82606131f31e1b5d1962b0b61726dfbdd3303087
timonson <timo.rossa@tutanota.com>
2022-06-29 16:58:19 +0200

    Improve for 'deno doc'

--- util.ts
+++ util.ts
@@ -1,5 +1,9 @@
 // deno-lint-ignore-file
 
+/*
+   * Async
+   */
+
 // https://stackoverflow.com/questions/70050001/javascript-how-to-compose-asynchronous-functions
 const compose2 = (f: any, g: any) =>
   async (...args: any) => f(await g(...args));
@@ -7,35 +11,25 @@ const compose2 = (f: any, g: any) =>
 // https://github.com/reduxjs/redux/blob/master/src/compose.ts
 type Func<T extends any[], R> = (...a: T) => R;
 
-/**
- * Composes single-argument functions from right to left. The rightmost
- * function can take multiple arguments as it provides the signature for the
- * resulting composite function.
- *
- * @param funcs The functions to compose.
- * @returns A function obtained by composing the argument functions from right
- *   to left. For example, `compose(f, g, h)` is identical to doing
- *   `(...args) => f(g(h(...args)))`.
- */
-export function compose(): <R>(a: R) => R;
+function compose1(): <R>(a: R) => R;
 
-export function compose<F extends Function>(f: F): F;
+function compose1<F extends Function>(f: F): F;
 
 /* two functions */
-export function compose<A, T extends any[], R>(
+function compose1<A, T extends any[], R>(
   f1: (a: A) => R,
   f2: Func<T, A>,
 ): Func<T, R>;
 
 /* three functions */
-export function compose<A, B, T extends any[], R>(
+function compose1<A, B, T extends any[], R>(
   f1: (b: B) => R,
   f2: (a: A) => B,
   f3: Func<T, A>,
 ): Func<T, R>;
 
 /* four functions */
-export function compose<A, B, C, T extends any[], R>(
+function compose1<A, B, C, T extends any[], R>(
   f1: (c: C) => R,
   f2: (b: B) => C,
   f3: (a: A) => B,
@@ -43,14 +37,14 @@ export function compose<A, B, C, T extends any[], R>(
 ): Func<T, R>;
 
 /* rest */
-export function compose<R>(
+function compose1<R>(
   f1: (a: any) => R,
   ...funcs: Function[]
 ): (...args: any[]) => R;
 
-export function compose<R>(...funcs: Function[]): (...args: any[]) => R;
+function compose1<R>(...funcs: Function[]): (...args: any[]) => R;
 
-export function compose(...funcs: Function[]) {
+function compose1(...funcs: Function[]) {
   if (funcs.length === 0) {
     // infer the argument type so it is usable in inference down the line
     return <T>(arg: T) => arg;
@@ -65,27 +59,40 @@ export function compose(...funcs: Function[]) {
 }
 
 /**
- * Composes non-async functions faster.
+ * Composes single-argument functions from right to left. The rightmost
+ * function can take multiple arguments as it provides the signature for the
+ * resulting composite function. It accepts sync and async functions.
+ *
+ * @param funcs The sync and async functions to compose.
+ * @returns A function obtained by composing the argument functions from right
+ *   to left. For example, `compose(f, g, h)` is identical to doing
+ *   `(...args) => f(g(h(...args)))`.
  */
-export function composeSync(): <R>(a: R) => R;
+export const compose = compose1; // Needs renaming for `deno doc`
 
-export function composeSync<F extends Function>(f: F): F;
+/*
+   * Sync
+   */
+
+function composeSync1(): <R>(a: R) => R;
+
+function composeSync1<F extends Function>(f: F): F;
 
 /* two functions */
-export function composeSync<A, T extends any[], R>(
+function composeSync1<A, T extends any[], R>(
   f1: (a: A) => R,
   f2: Func<T, A>,
 ): Func<T, R>;
 
 /* three functions */
-export function composeSync<A, B, T extends any[], R>(
+function composeSync1<A, B, T extends any[], R>(
   f1: (b: B) => R,
   f2: (a: A) => B,
   f3: Func<T, A>,
 ): Func<T, R>;
 
 /* four functions */
-export function composeSync<A, B, C, T extends any[], R>(
+function composeSync1<A, B, C, T extends any[], R>(
   f1: (c: C) => R,
   f2: (b: B) => C,
   f3: (a: A) => B,
@@ -93,14 +100,14 @@ export function composeSync<A, B, C, T extends any[], R>(
 ): Func<T, R>;
 
 /* rest */
-export function composeSync<R>(
+function composeSync1<R>(
   f1: (a: any) => R,
   ...funcs: Function[]
 ): (...args: any[]) => R;
 
-export function composeSync<R>(...funcs: Function[]): (...args: any[]) => R;
+function composeSync1<R>(...funcs: Function[]): (...args: any[]) => R;
 
-export function composeSync(...funcs: Function[]) {
+function composeSync1(...funcs: Function[]) {
   if (funcs.length === 0) {
     // infer the argument type so it is usable in inference down the line
     return <T>(arg: T) => arg;
@@ -112,3 +119,16 @@ export function composeSync(...funcs: Function[]) {
 
   return funcs.reduce((a, b) => (...args: any) => a(b(...args)));
 }
+
+/**
+ * Composes single-argument functions from right to left. The rightmost
+ * function can take multiple arguments as it provides the signature for the
+ * resulting composite function. The function `composeSync` doesn't accept async
+ * functions but composes non-async functions faster than `compose` does.
+ *
+ * @param funcs The sync functions to compose.
+ * @returns A function obtained by composing the argument functions from right
+ *   to left. For example, `compose(f, g, h)` is identical to doing
+ *   `(...args) => f(g(h(...args)))`.
+ */
+export const composeSync = composeSync1; // Needs renaming for `deno doc`
