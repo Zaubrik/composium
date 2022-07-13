b04626f72d0c1a289ef17078986c9e9bd3bc7043
timonson <timo.rossa@tutanota.com>
2022-07-13 06:58:50 +0200

    Add url pattern user interface

---
+++ url_pattern/deps.js
@@ -0,0 +1,10 @@
+export { LabeledControls, SvgButton } from "./pouch/mod.js";
+
+export {
+  cloneTemplateFragment,
+  createAndFillElement,
+  createTemplate,
+  fill,
+  getLastNode,
+  getShadowRootHost,
+} from "./sorcery/web/mod.js";
---
+++ url_pattern/server.ts
@@ -0,0 +1,16 @@
+import { Context, createHandler, createRoute, listen } from "../mod.ts";
+import { serveDir } from "https://deno.land/std@0.148.0/http/file_server.ts";
+
+function identity<X>(x: X) {
+  return x;
+}
+
+async function serveStatic(ctx: Context) {
+  ctx.response = await serveDir(ctx.request, { showDirListing: true });
+  return ctx;
+}
+
+const mainHandler = createRoute("GET")({ pathname: "*" })(serveStatic);
+const handler = createHandler(Context)(mainHandler)(identity)(identity);
+
+await listen({ port: 8080 })(handler);
