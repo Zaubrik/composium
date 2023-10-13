b3ab9508e442ee938f95b765d5b8ed2ff16ea1ee
timonson <timo.rossa@tutanota.com>
2023-10-13 19:30:58 +0200

    Update

--- test_deps.ts
+++ test_deps.ts
@@ -1,2 +1,2 @@
-export { assertEquals } from "https://deno.land/std@0.200.0/testing/asserts.ts";
-export { delay } from "https://deno.land/std@0.200.0/async/delay.ts";
+export { assertEquals } from "https://deno.land/std@0.202.0/testing/asserts.ts";
+export { delay } from "https://deno.land/std@0.202.0/async/delay.ts";
--- url-pattern/server.ts
+++ url-pattern/server.ts
@@ -1,6 +1,6 @@
 import { type Context, createDefaultHandler, createGetRoute } from "../mod.ts";
-import { serveDir } from "https://deno.land/std@0.192.0/http/file_server.ts";
-import { fromFileUrl } from "https://deno.land/std@0.192.0/path/mod.ts";
+import { serveDir } from "https://deno.land/std@0.202.0/http/file_server.ts";
+import { fromFileUrl } from "https://deno.land/std@0.202.0/path/mod.ts";
 
 async function serveStatic(ctx: Context) {
   ctx.response = await serveDir(ctx.request, {
