994b07c0ae718ce4dadc057a49f34919c4cdd2c4
timonson <timo.rossa@tutanota.com>
2024-05-02 14:25:33 +0200

    Bump

--- test_deps.ts
+++ test_deps.ts
@@ -1,2 +1,2 @@
-export { assertEquals } from "https://deno.land/std@0.212.0/testing/asserts.ts";
-export { delay } from "https://deno.land/std@0.212.0/async/delay.ts";
+export { assertEquals } from "https://deno.land/std@0.224.0/testing/asserts.ts";
+export { delay } from "https://deno.land/std@0.224.0/async/delay.ts";
